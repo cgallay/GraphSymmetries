521c24d239446789a5df981d318b157fc72ed957
Charles Gallay <charles.gallay@epfl.ch>
2019-03-06 11:32:03 +0100

    Set best param for Resnet18

--- run_best.sh
+++ run_best.sh
@@ -3,15 +3,14 @@
 # this script record the best parameter known for each architecture
 
 case $1 in
-"VGG")
-python3.6 main.py --arch $1 
-;;
-"ResNet")
-python3.6 main.py --arch $1
-;;
-*)
-echo "Best param not found for this model default param will be used."
-python3.6 main.py --arch $1
+    "VGG")
+        python3.6 main.py --arch $1 
+    ;;
+    "ResNet18")
+        python3.6 main.py --arch $1 --lr=0.1 --wd=5e-4 --da --nb_epochs=200
+    ;;
+    *)
+        echo "Best param not found for this model default param will be used."
+        python3.6 main.py --arch $1
+    ;;
 esac
-
-
