a4b839a02fa6039729b0ea300d430add23e5abcc
Charles Gallay <charles.gallay@epfl.ch>
2019-03-19 18:02:48 +0100

    Better param for training

--- run_best.sh
+++ run_best.sh
@@ -10,7 +10,7 @@ case $1 in
         python3.6 main.py --arch $1 --lr=0.1 --wd=5e-4 --da --nb_epochs=350 --ls 150 250 350
     ;;
     "ConvNet")
-        python3.6 main.py --arch $1 --lr=0.1 --wd=5e-4 --da --nb_epochs=160 --ls 75 125 150  
+        python3.6 main.py --arch $1 --lr=0.05 --wd=5e-4 --da --nb_epochs=160 --ls 75 125 150  
     ;;
     *)
         echo "Best param not found for this model default param will be used."
